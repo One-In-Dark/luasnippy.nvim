afc5567b0d99e807987b66f2a29dfbb10d251120
OneInDark <damonchiang.pku@qq.com>
2025-02-18 19:12:01 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
 # luasnippy.nvim
 A more ergonomic snippet facility wrapping LuaSnip
+
+# Rationale
+
+Snippets are powerful, but to utilize them maximally, one should write his own snippets. Otherwise it’s more a LSP thing.
+
+[LuaSnip](https://github.com/L3MON4D3/LuaSnip) has provided a good mechanism for *computers* to implement the snippet functionality, by structuring all information in Lua tables and breaking up the snippet into nodes. However, writing snippets directly in LuaSnip requires a lot of boilerplate. I really miss the neat format of UltiSnips.
+
+LuaSnippy aims to bring back the ergonomic way of snippets’ definition. An example should help illustrate this:
+
+```lua
+return packsnip {
+   snipa("b Ce A", "beg", [==[
+      \begin{<>}
+        <>
+      \end{<>}
+      ]==], { i(1), i(0), extras.rep(1) }),
+   cnd(IsInMath, {
+      snipa("iA P500", "//", "\\frac{<>}{<>}", { i(1), i(2) }),
+      snipa("iRv P500", "bar", "\\overline{<>}", { i(1) }),
+   }),
+}
+```
+
++ Grouping snippets by conditions (contexts)
++ Short-hand syntax for context options
++ Intuitive format-string syntax
+
+# Installation
+
+You can use any of the plugin manager, or `git clone` this repo and add it to your `'rtp'`. Using *lazy.nvim* we may also specify this plugin as a dependency of *LuaSnip* by something like this:
+
+```lua
+   {
+      "L3MON4D3/LuaSnip",
+      dependencies = {
+         { "One-In-Dark/luasnippy.nvim" },
+      },
+   }
+```
+
+# Usage
+
+## Snippet
+
+The main functionality of this plugin is provided in:
+
+```lua
+local snip = require("luasnippy").snippy
+```
+
+Normally the first argument of `snippy` is a string, with a similar syntax to UltiSnips, specifying the context of the snippet.
+
++ The common expansion conditions are provided:
+  + `i` stands for in-word, i.e. not matching the entire word (`[%w_]+`) before the cursor;
+  + `b` stands for beginning-of-line, i.e. only expand if the trigger word is at the beginning of the line;
+  + `Ce` stands for endding-of-line, i.e. only expand at the end of the line.
++ Commonly used is the regex matching. All three variants are supported (see `trigEngine` in `:h luasnip-snippets`):
+  + `r` stands for Lua pattern, which suffices most of the time, nevertheless the support of Unicode is poor;
+  + `Rv` stands for Vim-regex;
+  + `Re` stands for ECMAscript-regex.
++ Another two handy options:
+  + `A` stands for auto-expansion;
+  + `P<num>` sets the priority to `<num>` (see `priority` in `:h luasnip-snippets`).
+
+The second argument is the trigger word. The next two arguments form a f-string in Python (for those who are unfamiliar, it’s like string interpolation in shell), as is illustrated by the example above.
+
+The last argument can further regulate the behaviour of the string formatting, see `opts` in `:h luasnip-extras-fmt`.
+
+A variant that uses angular brackets `<>` for string formatting instead of `{}` is provided:
+
+```lua
+local snipa = require("luasnippy").snippy_angular
+```
+
+## Conditional
+
+```lua
+local cnd = require("luasnippy").conditional_by
+```
+
+Conditions apply to all snippets in its scope, which makes it very easy to write now. `conditional_by` accepts a function or a condition object (see `CONDITION OBJECTS` in `:h luasnip-extras-conditions`), and applies it to the snippets in the second argument.
+
+Nesting is supported.
+
+## Backend
+
+LuaSnip is the “backend” of the plugin. The function `pack_snippets` can “compile” LuaSnippy-style snippets:
+
+```lua
+local packsnip = require("luasnippy").pack_snippets
+```
+
+Now enjoy the power of snippets!
+
---
+++ lua/luasnippy.lua
@@ -0,0 +1,140 @@
+local luasnip = require("luasnip")
+local fmt = require("luasnip.extras.fmt").fmt
+local conditions = require("luasnip.extras.conditions")
+local conditionsExpand = require("luasnip.extras.conditions.expand")
+
+---@return fun(_, parent): any # a function that extracts the capture with the given index from the parent snippet, for use in function node.
+local function capture_extract(indice)
+   return function (_, parent)
+      return parent.snippet.captures[indice]
+   end
+end
+
+---@param context string The context of the snippet, complying with the short-handed syntax described in `snippy`
+---@param baseset table The base set of options to be used for the snippet, which should be mutable
+local function parse_context(context, baseset)
+   local context_tbl = baseset
+   if context:match("r") then
+      context_tbl.trigEngine = "pattern"
+   end
+   if context:match("Rv") then
+      context_tbl.trigEngine = "vim"
+   end
+   if context:match("Re") then
+      context_tbl.trigEngine = "ecma"
+   end
+
+   if context:match("i") then
+      context_tbl.wordTrig = false
+   end
+   if context:match("b") then
+      context_tbl.condition = context_tbl.condition
+          and context_tbl.condition + conditionsExpand.line_begin
+          or conditionsExpand.line_begin
+   end
+   if context:match("Ce") then
+      context_tbl.condition = context_tbl.condition
+          and context_tbl.condition + conditionsExpand.line_end
+          or conditionsExpand.line_end
+   end
+
+   if context:match("A") then
+      context_tbl.snippetType = "autosnippet"
+   end
+   context_tbl.priority = context:match("P(%d+)")
+   if context_tbl.priority then
+      context_tbl.priority = tonumber(context_tbl.priority)
+   end
+   return context_tbl
+end
+
+---@class (exact) SnippetTuple
+---@field [1] table The context of the snippet
+---@field [2] string Contents of the snippet as in `fmt`
+---@field [3] any The body elements of the snippet
+---@field [4] table Table to be passed to `fmt`
+---@field _snip boolean Set for all `SnippetTuple` instance
+
+---Creates a LuaSnippy snippet, supporting the following short-handed option syntax:
+---- `r` for Lua pattern, `Rv` for vim regex, `Re` for ECMAscript regex,
+---- `i` for in-word, `b` for beginning-of-line, `Ce` for endding-of-line,
+---- `A` for autosnippet, `P<num>` for priority `<num>`.
+---Any other characters are ignored.
+---@param context table|string The context of the snippet (see `context` in `:h luasnip-snippets`), additionally supporting short-handed option syntax described above, either as a single string or as the field `[1]` of the table.
+---@param trigger string
+---@param body_str string The contents of the snippet, as in `fmt` (see `format` in `:h luasnip-extras-fmt`)
+---@param body_elems any|nil The body elements of the snippet, as in `fmt` (see `nodes` in `:h luasnip-extras-fmt`)
+---@param opts table|nil Passed to `fmt` as the third argument (see `opts` in `:h luasnip-extras-fmt`)
+---@return SnippetTuple # The snippet tuple with the given context, trigger, and body elements
+local function snippy(context, trigger, body_str, body_elems, opts)
+   local context_tbl
+   if type(context) == "table" then
+      if context[1] then
+         assert(type(context[1]) == "string",
+            "Invalid context[1] type: " .. type(context[1]))
+         context_tbl = parse_context(context[1], context)
+      else context_tbl = context end
+   elseif type(context) == "string" then
+      context_tbl = parse_context(context, {})
+   else error("Invalid context type: " .. type(context)) end
+   context_tbl.trig = trigger
+   body_elems = body_elems or {}
+   return { context_tbl, body_str, body_elems, opts, _snip = true }
+end
+
+---Works the same way as `snippy`, with delimiters set to angular brackets "<>". It to `snippy` is what `fmta` to `fmt`, see `:h luasnip-extras-fmt`.
+local function snippy_angular(context, trigger, body_str, body_elems, extra)
+   extra = extra or {}
+   extra.delimiters = "<>"
+   return snippy(context, trigger, body_str, body_elems, extra)
+end
+
+---@param cond table|function A function, or a condition object (see `CONDITION OBJECTS` in `:h luasnip-extras-conditions`).
+---@param snippets (SnippetTuple|SnippetTuple[])[]
+---@return SnippetTuple[] # The snippets that respects the condition
+local function conditional_by(cond, snippets)
+   if type(cond) == "function" then
+      cond = conditions.make_condition(cond)
+   elseif type(cond) ~= "table" then
+      error("Invalid condition type: " .. type(cond))
+   end
+   local snippetsConditional = {}
+   for _, snipgrp in ipairs(snippets) do
+      if snipgrp._snip then
+         snipgrp = { snipgrp } -- singleton list
+      end
+      for _, snip in ipairs(snipgrp --[[@as SnippetTuple[]--]]) do
+         assert(snip._snip, "Non-snippet passed to conditional_by")
+         snip[1].condition = snip[1].condition
+            and snip[1].condition * cond or cond
+         table.insert(snippetsConditional, snip)
+      end
+   end
+   return snippetsConditional
+end
+
+---Accepts a list of LuaSnippy snippets and converts them to LuaSnip snippets, for use in snippet file `return` (see `:h luasnip-loaders-lua`) or `luasnip.add_snippets` (see `:h luasnip-api`).
+---@param snippetTuples (SnippetTuple|SnippetTuple[])[]
+---@return any # list of LuaSnip snippets
+local function pack_snippets(snippetTuples)
+   local snippets = {}
+   for _, snipgrp in ipairs(snippetTuples) do
+      if snipgrp._snip then
+         snipgrp = { snipgrp } -- singleton list
+      end
+      for _, snip in ipairs(snipgrp --[[@as SnippetTuple[]--]]) do
+         assert(snip._snip, "Non-snippet passed to pack_snippets")
+         snip = luasnip.snippet(snip[1], fmt(snip[2], snip[3], snip[4]))
+         table.insert(snippets, snip)
+      end
+   end
+   return snippets
+end
+
+return {
+   snippy = snippy,
+   snippy_angular = snippy_angular,
+   conditional_by = conditional_by,
+   capture_extract = capture_extract,
+   pack_snippets = pack_snippets,
+}
